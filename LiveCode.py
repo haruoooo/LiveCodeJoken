4b12b9ba5c69b938c50405b54b039290884bcb81
Matheus Haruo <haruo.fiap@gmail.com>
2025-02-12 20:55:21 -0300

    Fim do teste.

--- LiveCode.py
+++ LiveCode.py
@@ -36,7 +36,7 @@ def determine_winner(user_choice, computer_choice):
 
 def main():
     """
-    Função principal que executa o jogo. Teste teste teste
+    Função principal que executa o jogo. 
     """
     print("\nBem-vindo ao Jokenpô!")
     
